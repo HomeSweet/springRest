23f1625a067b71ccf8a8e5914a4893692baa28c6
sergey <sergey@DESKTOP-DJUEO96>
2018-03-03 15:49:15 +0200

    Task #7.

--- rest-task5/src/main/java/it/discovery/controller/BookController.java
+++ rest-task5/src/main/java/it/discovery/controller/BookController.java
@@ -1,22 +1,47 @@
 package it.discovery.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.discovery.model.Book;
+import it.discovery.repository.BookRepository;
 
 @RestController
 @RequestMapping("/book")
-public class BookController {	
+public class BookController {
 	
-	@GetMapping(produces= {MediaType
-			.APPLICATION_JSON_UTF8_VALUE,
-			MediaType.APPLICATION_XML_VALUE})
-	public Book getEmptyBok() {
-		return new Book();
+	private final BookRepository bookRepository;
+
+	public BookController(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
 	}
 
+	@GetMapping
+	public List<Book> getBooks() {
+		return bookRepository.findAll();
+	}
+
+	@GetMapping("{/id}")
+	public Book findBookById(@PathVariable int id) {
+		return bookRepository.findById(id);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void saveBook(@Valid @RequestBody Book book) {
+		bookRepository.save(book);
+	}
 
 }
