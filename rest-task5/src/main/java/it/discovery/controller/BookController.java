a87775a738d157456d1bf1625a04a85e49e9bf20
sergey <sergey@DESKTOP-DJUEO96>
2018-03-03 15:07:33 +0200

    Task #5

--- rest-task5/src/main/java/it/discovery/controller/BookController.java
+++ rest-task5/src/main/java/it/discovery/controller/BookController.java
@@ -1,11 +1,22 @@
 package it.discovery.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import it.discovery.model.Book;
 
 @RestController
 @RequestMapping("/book")
 public class BookController {	
+	
+	@GetMapping(produces= {MediaType
+			.APPLICATION_JSON_UTF8_VALUE,
+			MediaType.APPLICATION_XML_VALUE})
+	public Book getEmptyBok() {
+		return new Book();
+	}
 
 
 }
