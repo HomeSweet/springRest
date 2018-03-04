8b6df4ada456cbe0676fcf3e815e128412c4fa7c
sergey <sergey@DESKTOP-DJUEO96>
2018-03-04 11:50:21 +0200

    Task #1. Added unit-tests

--- rest-task10/src/test/java/it/discovery/controller/BookControllerTest.java
+++ rest-task10/src/test/java/it/discovery/controller/BookControllerTest.java
@@ -1,19 +1,21 @@
 package it.discovery.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.context.WebApplicationContext;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import it.discovery.bootstrap.RestApplication;
+import it.discovery.repository.BookRepository;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.Collections;
 
 import org.hamcrest.Matchers;
 
@@ -21,18 +23,19 @@ import org.hamcrest.Matchers;
 @AutoConfigureMockMvc
 public class BookControllerTest {
 	@Autowired
-    private MockMvc mockMvc;
-    
-    private static final ObjectMapper MAPPER = new ObjectMapper();
+	private MockMvc mockMvc;
 
-    @Test
-    public void getBooks_RepositoryEmpty_NoBooksReturned() throws Exception { 
-    	mockMvc.perform(get("/book"))
-    	.andExpect(status().isOk())
-    	.andExpect(content().contentType(
-    			MediaType.APPLICATION_JSON_UTF8_VALUE))
-    	.andExpect(jsonPath("$", Matchers.hasSize(0)));
-    }
+	@MockBean
+	private BookRepository bookRepository;
+
+	@Test
+	public void getBooks_RepositoryEmpty_NoBooksReturned() throws Exception {
+
+		BDDMockito.given(bookRepository.findAll())
+		.willReturn(Collections.emptyList());
+		mockMvc.perform(get("/book")).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(jsonPath("$", Matchers.hasSize(0)));
+	}
 
 }
-
