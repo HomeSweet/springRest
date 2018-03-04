2ef0c9cc413183462403a3d9ae2153d07d6d1d66
sergey <sergey@DESKTOP-DJUEO96>
2018-03-04 15:16:03 +0200

    Task #14. Added Security configuration

---
+++ rest-task10/src/main/java/it/discovery/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package it.discovery.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration extends 
+	WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.authorizeRequests().anyRequest()
+		.fullyAuthenticated();
+	}
+
+//	@Override
+//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//		auth.inMemoryAuthentication()
+//		.withUser("admin")
+//		.password("admin")
+//		.roles("ROLE_ADMIN")
+//		.and()
+//		.withUser("user").password("user").roles("ROLE_USER");
+//	}
+	
+	
+}
